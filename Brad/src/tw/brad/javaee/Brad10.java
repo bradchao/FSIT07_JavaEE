bceb771f9ee8b8bde41df37f3b6d6cd073adc34c
user <user@10.0.103.39>
2020-02-02 11:40:57 +0800

    2002021140

--- Brad/src/tw/brad/javaee/Brad10.java
+++ Brad/src/tw/brad/javaee/Brad10.java
@@ -12,17 +12,37 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/Brad10")
 public class Brad10 extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
+		
+		request.setCharacterEncoding("UTF-8");
+		String x = request.getParameter("x");
+		String y = request.getParameter("y");
+		int intX, intY, result; 
+		result = intX = intY = 0;
+		
+		if (x != null) {
+			try {
+				intX = Integer.parseInt(x);
+				intY = Integer.parseInt(y);
+				result = intX + intY;
+			}catch(Exception e) {}
+		}
+		
 		response.setContentType("text/html; charset=UTF-8");
 		PrintWriter out = response.getWriter();
 		
 		out.append("<h1>Brad Big Company</h1>")
 		.append("<hr />")
-		.append("<form action='Brad09' method='get'>")
-		.append("<input type='text' name='x'>")
-		.append("+")
-		.append("<input type='text' name='y'>")
+		.append("<form action='Brad10' method='get'>")
+		.append("<input type='text' name='x' value='" + intX + "'>")
+		.append("<select>")
+		.append("<option>+</option>")
+		.append("<option>-</option>")
+		.append("<option>x</option>")
+		.append("<option>/</option>")
+		.append("</select>")
+		.append("<input type='text' name='y' value='" + intY + "'>")
 		.append("<input type='submit' value='=' />")
+		.append("" + result)
 		.append("</form>");
 		response.flushBuffer();
 	
