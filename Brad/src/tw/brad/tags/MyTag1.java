343dadecd508b9c50df4fc30c5b43c138aded58e
user <user@WTWANG-MBP.r1804.tciii.org.tw>
2020-02-23 10:35:37 +0800

    02231035

---
+++ Brad/src/tw/brad/tags/MyTag1.java
@@ -0,0 +1,23 @@
+package tw.brad.tags;
+
+import java.io.IOException;
+
+import javax.servlet.jsp.JspContext;
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.SimpleTagSupport;
+
+public class MyTag1 extends SimpleTagSupport{
+	@Override
+	public void doTag() throws JspException, IOException {
+		super.doTag();
+		
+		System.out.println("debug1");
+		JspContext context = getJspContext();
+		JspWriter out = context.getOut();
+		out.println("Hello, Brad");
+		
+		
+	}
+
+}
